69f8831ccc4256e4866320340e4faaa16936c13a
Andrey Vershinin <adw006@ya.ru>
2018-04-15 22:17:04 +0300

    prototypes

---
+++ prototypes/Node.js
@@ -0,0 +1,17 @@
+function getAttributesAsLine() {
+  return Object.keys(this.attributes).reduce((acc, key) =>
+    `${acc} ${key}="${this.attributes[key]}"`, '');
+}
+
+export default function Node(name, attributes = {}) {
+  this.name = name;
+  this.attributes = attributes;
+
+  this.getAttributesAsLine = getAttributesAsLine;
+}
+
+// === прототипное наследование ==============
+Node.prototype.getAttributesAsLine = function getAttributesAsLine() {
+  return Object.keys(this.attributes).reduce((acc, key) =>
+    `${acc} ${key}="${this.attributes[key]}"`, '');
+};
---
+++ prototypes/SingleTag.js
@@ -0,0 +1,22 @@
+import Node from './Node';
+
+export default function SingleTag(name, attributes = {}) {
+  Node.apply(this, [name, attributes]);
+}
+
+SingleTag.prototype.toString = function toString() {
+  return `<${this.name}${this.getAttributesAsLine()}>`;
+};
+
+// === прототипное наследование ==============
+import Node from './Node';
+
+export default function SingleTag(name, attributes = {}) {
+  Node.apply(this, [name, attributes]);
+}
+
+SingleTag.prototype = Object.create(Node.prototype);
+
+SingleTag.prototype.toString = function toString() {
+  return `<${this.name}${this.getAttributesAsLine()}>`;
+};
